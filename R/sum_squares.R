318f784d07c38bddc4663be5cb01f62caca693ae
Valentina Marconi <valentina.marconi@ioz.ac.uk>
2020-01-07 12:22:40 +0000

    Function documentation

--- R/sum_squares.R
+++ R/sum_squares.R
@@ -1,3 +1,15 @@
+#' Calculate the sums of squares for a sample
+#'
+#'returns the sum of the squared deviations
+#'from mean, SS(x) of a numeric vector
+#'
+#' @param v numeric vector
+#'
+#' @return number
+#' @export
+#'
+#' @examples
+#' sum_squares(rnorm(50))
 sum_squares <- function(v) {
   sum((v-mean(v))^2)
 }
